dc7f4b719ce98c9a0a01bdf623c5296e5a804574
Prakash Kumar <kumarprakash0196@gmail.com>
2025-06-30 16:44:51 +0530

    text-commit

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -14,9 +14,19 @@ export const MainSection=()=>{
             <div className="row py-5">
                 <div className="col-lg-6 py-5 mr-auto">
                     <h3><small>Hello I'm</small><br />
-                        <span>Prakash Kumar</span>
+                        <span>Prakash Kumar....</span>
                     </h3>
-                    <h6 className="pt-3 web">Web_Developer  </h6>
+                    {/* <h6 className="pt-3 web">Web_Developement  </h6> */}
+                   <div class="text-changer">
+    <p class="intro-text">I am a</p>
+    <div class="roles-container">
+      <span class="role"> <h6>web development</h6>  </span>
+      <span class="role"> <h6>react developer</h6> </span>
+      <span class="role"> <h6>mern developer</h6></span>
+      <span class="role"> <h6>full stack developer</h6> </span>
+      {/* <span class="role"> <h6>lover</h6> </span> */}
+    </div>
+  </div>
                     <div className="gap-2">
                     <a href="#blog"><input type="button" value="My Work" className="bt1 mt-5 mr-3" style={{marginRight: "5px"}} /></a>
                     <a href="#contact"><input type="button" value=" Hire Me" className="bt2 mt-5" id="" /></a>
