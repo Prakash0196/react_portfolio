a9e56513492947e4306ac460b93b1886eec849b9
Prakash Kumar <kumarprakash0196@gmail.com>
2025-06-30 20:40:02 +0530

    typewriter-commit

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -5,8 +5,11 @@ import "./index.css"
 import { MainButton } from "./Main_button"
 import { Project } from "./Projects"
 import { Services } from "./Services"
+import { Typewriter } from 'react-simple-typewriter';
+
 
 export const MainSection=()=>{
+
     return (
         <>
         <section className="main py-5">
@@ -17,16 +20,33 @@ export const MainSection=()=>{
                         <span>Prakash Kumar....</span>
                     </h3>
                     {/* <h6 className="pt-3 web">Web_Developement  </h6> */}
-                   <div class="text-changer">
+
+                   {/* <div class="text-changer">
     <p class="intro-text">I am a</p>
     <div class="roles-container">
       <span class="role"> <h6>web development</h6>  </span>
       <span class="role"> <h6>react developer</h6> </span>
       <span class="role"> <h6>mern developer</h6></span>
       <span class="role"> <h6>full stack developer</h6> </span>
-      {/* <span class="role"> <h6>lover</h6> </span> */}
     </div>
-  </div>
+  </div> */}
+ 
+
+ <h6 id="profession" style={{color: "#3F51B5"}}>
+      <span style={{ color: 'red', fontWeight: 'bold' }}>
+        <Typewriter
+          words={['Web Development', 'React Developer', 'Freelancer', 'Mern Developer']}
+          loop={true}
+          cursor
+          cursorColor="#3F51B5"
+          cursorStyle='|'
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+      </span>
+    </h6>
+
                     <div className="gap-2">
                     <a href="#blog"><input type="button" value="My Work" className="bt1 mt-5 mr-3" style={{marginRight: "5px"}} /></a>
                     <a href="#contact"><input type="button" value=" Hire Me" className="bt2 mt-5" id="" /></a>
